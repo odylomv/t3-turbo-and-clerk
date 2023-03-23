32c4154d40963d79f9f0c33de42058678e757066
odylomv <odylomv@gmail.com>
2023-03-23 15:25:01 +0200

    better prettier config typecheck

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,8 @@
-/** @type {import("prettier").Config & { [key:string]: any }} */
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
   arrowParens: 'avoid',
   printWidth: 120,
