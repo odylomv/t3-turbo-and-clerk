2d5af3eb40a511cafab9e0d9ed0f2c5989032b7d
odylomv <odylomv@gmail.com>
2023-03-24 19:01:01 +0200

    redesign nextjs page based on newer t3-turbo

--- apps/nextjs/src/pages/sign-in/[[...index]].tsx
+++ apps/nextjs/src/pages/sign-in/[[...index]].tsx
@@ -2,11 +2,11 @@ import { SignIn } from '@clerk/nextjs';
 
 const SignInPage = () => (
   <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    <div className="flex flex-col items-center justify-center gap-4">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">Sign In</h1>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-      </div>
+    <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
+      <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        Create <span className="text-pink-400">T3</span> Turbo
+      </h1>
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
     </div>
   </main>
 );
